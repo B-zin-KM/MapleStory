00ea91f61c1db2c3a77c67ca2f1f1b7d653151c5
B-zin-KM <165834109+B-zin-KM@users.noreply.github.com>
2024-11-11 03:03:48 +0900

    공격모션 구현완료 (궁극의 상태머신)

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from pico2d import load_image, SDL_KEYDOWN, SDLK_RIGHT, SDLK_LEFT, SDLK_LALT, SDLK_a, SDL_KEYUP, SDLK_DOWN, get_time, draw_rectangle
+from pico2d import load_image, SDL_KEYDOWN, SDLK_RIGHT, SDLK_LEFT, SDLK_LALT, SDLK_LCTRL, SDLK_a, SDL_KEYUP, SDLK_DOWN, get_time, draw_rectangle
 import math
 
 
@@ -26,11 +26,14 @@ def alt_down(e):
 def alt_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LALT
 
+def ctrl_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LCTRL
+
+def ctrl_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LCTRL
+
 def a_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_a
-
-def a_up(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_a
 
 
 class Idle:
@@ -41,9 +44,9 @@ class Idle:
 
     @staticmethod
     def enter(player, e):
-        if player.action == 1 or player.action == 5 or player.action == 7:
+        if player.action == 1 or player.action == 5 or player.action == 7 or player.action == 9:
             player.action, player.dir = 3, 1
-        elif player.action == 2 or player.action == 6 or player.action == 8:
+        elif player.action == 2 or player.action == 6 or player.action == 8 or player.action == 10:
             player.action, player.dir = 4, -1
         player.frame = 0
         Idle.elapsed_time = 0.0
@@ -111,15 +114,20 @@ class Walk:
 
     @staticmethod
     def enter(player, e):
-        if right_down(e) or left_up(e):
-            player.dir, player.action = 1, 1
-        elif left_down(e) or right_up(e):
-            player.dir, player.action = -1, 2
-        elif down_up(e):
-            if player.dir == 1:
-                player.action = 1
-            if player.dir == -1:
-                player.action = 2
+        if e is not None:
+            if right_down(e) or left_up(e):
+                player.dir, player.action = 1, 1
+            elif left_down(e) or right_up(e):
+                player.dir, player.action = -1, 2
+        else:
+            if player.right_key_pressed:
+                player.dir = 1
+            elif player.left_key_pressed:
+                player.dir = -1
+        if player.dir == 1:
+            player.action = 1
+        if player.dir == -1:
+            player.action = 2
         Walk.elapsed_time = 0.0
         Walk.last_time = get_time()
 
@@ -213,18 +221,85 @@ class Prone:
                     player.image.clip_draw(19, 653, 41, 66, player.x + 10, player.y)
             else:
                 if player.action == 7:
-                    player.x = player.x + 23
-                    player.image.clip_draw(player.frame * 78, 575, 64, 39, player.x, player.y - 15)
-                    player.x = player.x - 23
+                    player.image.clip_draw(78, 575, 64, 39, player.x + 23, player.y - 15)
                 elif player.action == 8:
-                    player.x = player.x - 5
-                    player.image.clip_draw(player.frame * 78, 679, 64, 39, player.x, player.y - 15)
-                    player.x = player.x + 5
+                    player.image.clip_draw(78, 679, 64, 39, player.x - 5, player.y - 15)
         if player.char == 1:
-            pass
+            if player.air:
+                if player.dir == 1:
+                    player.image.clip_draw(19, 771, 50, 66, player.x + 5, player.y)
+                elif player.dir == -1:
+                    player.image.clip_draw(19, 875, 50, 66, player.x + 14, player.y)
+            else:
+                if player.action == 7:
+                        player.image.clip_draw(87, 797, 68, 39, player.x + 25, player.y - 15)
+                elif player.action == 8:
+                        player.image.clip_draw(87, 901, 68, 39, player.x - 7, player.y - 15)
 
 class Attack:
-    pass
+
+    idle_time = 0.15
+    elapsed_time = 0.0
+    last_time = 0.0
+
+    @staticmethod
+    def enter(player, e):
+        if player.dir == 1:
+            player.dir, player.action = 1, 9
+        elif player.dir == -1:
+            player.dir, player.action = -1, 10
+        Attack.elapsed_time = 0.0
+        Attack.last_time = get_time()
+        player.frame = 0
+        player.attack = True
+
+    @staticmethod
+    def exit(player, e):
+        player.attack = False
+        pass
+
+    @staticmethod
+    def do(player):
+        current_time = get_time()
+        time_difference = current_time - Attack.last_time
+        Attack.last_time = current_time
+        Attack.elapsed_time += time_difference
+
+        if Attack.elapsed_time >= Attack.idle_time:
+            Attack.elapsed_time = 0
+            player.frame = (player.frame + 1) % 4
+            if not player.ctrl_key_pressed:
+                if player.frame == 0:
+                    if player.left_key_pressed or player.right_key_pressed:
+                        player.state_machine.cur_state.exit(player, None)
+                        player.state_machine.cur_state = Walk
+                        player.state_machine.cur_state.enter(player, None)
+                    else:
+                        player.state_machine.cur_state.exit(player, None)
+                        player.state_machine.cur_state = Idle
+                        player.state_machine.cur_state.enter(player, None)
+
+    @staticmethod
+    def draw(player):
+        if player.action == 9:
+            if player.frame == 0:
+                player.image.clip_draw(80, 24, 47, 68, player.x + 15, player.y)
+            if player.frame == 1:
+                player.image.clip_draw(80, 24, 47, 68, player.x + 15, player.y)
+            elif player.frame == 2:
+                player.image.clip_draw(19, 26, 42, 66, player.x + 20, player.y - 1)
+            elif player.frame == 3:
+                player.image.clip_draw(145, 19, 71, 73, player.x + 8, player.y - 8)
+
+        elif player.action == 10:
+            if player.frame == 0:
+                player.image.clip_draw(80, 135, 47, 68, player.x + 4, player.y)
+            if player.frame == 1:
+                player.image.clip_draw(80, 135, 47, 68, player.x + 4, player.y)
+            elif player.frame == 2:
+                player.image.clip_draw(19, 137, 42, 66, player.x - 3, player.y - 1)
+            elif player.frame == 3:
+                player.image.clip_draw(145, 130, 71, 73, player.x + 10, player.y - 8)
 
 
 class StateMachine:
@@ -232,8 +307,8 @@ class StateMachine:
         self.player = player
         self.cur_state = Idle
         self.transition = {
-            Idle: {right_down: Walk, left_down: Walk, right_up: Walk, left_up: Walk, down_down: Prone, down_up: Idle},
-            Walk: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
+            Idle: {right_down: Walk, left_down: Walk, right_up: Walk, left_up: Walk, down_down: Prone, down_up: Idle, ctrl_down: Attack},
+            Walk: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle, ctrl_down: Attack},
             Prone: {down_down: Prone, down_up: Idle, right_down: Walk, left_down: Walk, right_up: Idle, left_up: Idle},
             Attack: {}
         }
@@ -248,6 +323,8 @@ class StateMachine:
         self.cur_state.draw(self.player)
 
     def handle_event(self, e):
+        if self.player.char == 0 and (e[1].key == SDLK_LCTRL and e[1].type == SDL_KEYDOWN):
+            return
         if self.cur_state == Walk and (e[1].key == SDLK_DOWN and e[1].type == SDL_KEYDOWN):
             return
         if (e[1].key == SDLK_LEFT or e[1].key == SDLK_RIGHT) and e[1].type == SDL_KEYUP:
@@ -281,6 +358,7 @@ class Player:
         self.dir = -1
         self.air = False
         self.jumping = False
+        self.attack = False
         self.gravity = -0.35            # 중력 가속도, 하강 시 속도가 점차 증가
         self.velocity = 0               # 현재 속도
         self.max_jump_velocity = 7.3    # 점프 시 초기 속도
@@ -288,6 +366,7 @@ class Player:
         self.left_key_pressed = False
         self.down_key_pressed = False
         self.alt_key_pressed = False
+        self.ctrl_key_pressed = False
 
     def jump(self):
         if not self.jumping and not self.air:
@@ -295,7 +374,7 @@ class Player:
             self.velocity = self.max_jump_velocity
 
     def update(self):
-        if self.state_machine.cur_state != Idle and not (self.right_key_pressed or self.left_key_pressed or self.down_key_pressed or self.alt_key_pressed):
+        if self.state_machine.cur_state != Idle and self.state_machine.cur_state != Attack and not (self.right_key_pressed or self.left_key_pressed or self.down_key_pressed or self.alt_key_pressed or self.ctrl_key_pressed):
             self.state_machine.cur_state = Idle
             self.state_machine.cur_state.enter(self, None)
 
@@ -328,6 +407,8 @@ class Player:
                 self.right_key_pressed = True
             elif event.key == SDLK_LEFT:
                 self.left_key_pressed = True
+            elif event.key == SDLK_LCTRL:
+                self.ctrl_key_pressed = True
             elif event.key == SDLK_a:
                 self.image = load_image('warrior_1.png')
                 self.char = 1
@@ -342,6 +423,8 @@ class Player:
                 self.right_key_pressed = False
             elif event.key == SDLK_LEFT:
                 self.left_key_pressed = False
+            elif event.key == SDLK_LCTRL:
+                self.ctrl_key_pressed = False
 
         self.state_machine.handle_event(('INPUT', event))
 
